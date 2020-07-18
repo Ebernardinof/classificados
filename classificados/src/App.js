7d010b491a4cb0c5124f78b381759a3cf0137305
Elisabete <38789950+Ebernardinof@users.noreply.github.com>
2020-07-18 20:36:49 +0100

    first commit

---
+++ classificados/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import { Switch, Route } from "react-router-dom";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/cart/Cart";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal />
+      <Default />
+    </>
+  );
+}
+
+export default App;
